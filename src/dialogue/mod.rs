cbd0064e83398d9ac337e158304cf61e20bc9fed
Nilsiker <nilsiker94@gmail.com>
2022-10-03 23:04:47 +0200

    clippy refactor, add cursorplugin and start on interaction

--- src/dialogue/mod.rs
+++ src/dialogue/mod.rs
@@ -2,63 +2,48 @@
 use bevy::prelude::*;
 
 use self::ui::DialogueUiPlugin;
+mod talker;
 mod ui;
 
 pub struct DialoguePlugin;
 impl Plugin for DialoguePlugin {
     fn build(&self, app: &mut App) {
-
-        let next_line =  DialogueNode::Line(DialogueData {  // TODO add None when done debugging here
+        let next_line = DialogueNode::Line(DialogueData {
+            // TODO add None when done debugging here
             text: "And here comes the next line".into(),
-            progress: 0,
             event: None,
             next: Box::new(DialogueNode::None),
         });
 
-        app.add_event::<DialogueChangeEvent>()
+        app
         .insert_resource(DialogueNode::Line(DialogueData {  // TODO add None when done debugging here
             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tincidunt congue quam, sed suscipit leo bibendum et. Mauris convallis varius tortor, sed vestibulum ligula efficitur a. Proin dignissim lectus nec dui rutrum, non varius nisl tincidunt. Aliquam justo dolor, consectetur eget feugiat ac, porta ut sem. Vivamus convallis ac nisl in ornare.".into(),
-            progress: 0,
             event: None,
             next: Box::new(next_line)
         }))
-        .add_system(handle_dialogue_change_events)
         .add_plugin(DialogueUiPlugin);
     }
 }
 
-pub struct DialogueChangeEvent(DialogueNode);
-
-fn handle_dialogue_change_events(
-    mut dialogue: ResMut<DialogueNode>,
-    mut events: EventReader<DialogueChangeEvent>,
-) {
-    events.iter().for_each(|event| {
-        *dialogue = event.0.clone(); // TODO verify that this clone is necessary and without side effects
-        bevy::log::info!("dialogue changed");
-    })
-}
-
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum DialogueNode {
     None,
     Line(DialogueData),
     Choice(Vec<DialogueData>),
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct DialogueData {
     text: String,
-    progress: usize,
     event: Option<DialogueEvent>,
     next: Box<DialogueNode>,
 }
 
-#[derive(Clone)]
+#[derive(Clone,Debug)]
 enum DialogueEvent {
     ItemReceived(Item),
     ItemLost(Item),
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Item;
